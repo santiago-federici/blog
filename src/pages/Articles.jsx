02fc9e169f70cfaadae9821ecff379e267085812
santiago-federici <santiagofederici88@gmail.com>
2023-06-10 10:10:28 -0300

    Created and styled the articles page for mobile and desktop

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,8 +1,98 @@
+
+import  Raptor  from '../assets/images/raptor.jpg'
+import  User  from '../assets/images/user.jpg'
+import  Bodybuilder  from '../assets/images/bodybuilder.jpg'
+import  Astronaut  from '../assets/images/astronaut.jpg'
+import { TrendingTopics } from '../components/trendingTopics/TrendingTopics'
 import './articles.css'
 
 export function Articles() {
+
+
     return (
-        <main>\
+        <main className='articles-main'>
+            <input type="text" placeholder='Search a topic' className='searchbox' />
+
+            <h2>For you</h2>
+            
+            <div className='articles-cards-container'>
+                <section className='article-card'>
+
+                    <img src={ Raptor } alt="" className='article-card__img'/>
+
+                    <div className='article-card__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                    </div>
+
+                    <section className='article-card__info'>
+                        <h2>Title</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam facilis pariatur modi eum harum dolorem...</p>
+                        <button className='readmore-btn'>Read more</button>
+                    </section>
+                </section>
+
+                <section className='article-card'>
+
+                    <img src={ Bodybuilder } alt="" className='article-card__img'/>
+
+                    <div className='article-card__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                    </div>
+
+                    <section className='article-card__info'>
+                        <h2>Title</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam facilis pariatur modi eum harum dolorem...</p>
+                        <button className='readmore-btn'>Read more</button>
+                    </section>
+                </section>
+
+                <section className='article-card'>
+
+                    <img src={ Astronaut } alt="" className='article-card__img'/>
+
+                    <div className='article-card__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                    </div>
+
+                    <section className='article-card__info'>
+                        <h2>Title</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam facilis pariatur modi eum harum dolorem...</p>
+                        <button className='readmore-btn'>Read more</button>
+                    </section>
+                </section>
+            </div>
+
+
+            <TrendingTopics />
+            
+
+            <div className='trending-writers'>
+                <h2>Trending Writers</h2>
+
+                <div className='trending-writers__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                        <button className='follow-btn'>Follow</button>
+                </div>
+                <div className='trending-writers__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                        <button className='follow-btn'>Follow</button>
+                </div>
+                <div className='trending-writers__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                        <button className='follow-btn'>Follow</button>
+                </div>
+                <div className='trending-writers__user'>
+                        <img src={ User } alt="" />
+                        <p>@userat</p>
+                        <button className='follow-btn'>Follow</button>
+                </div>
+            </div>
         </main>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { BsFillEnvelopeFill } from "react-icons/bs";
 import { CiMoneyCheck1 } from "react-icons/ci";
+import { TrendingTopics } from "../components/trendingTopics/TrendingTopics";
 import './home.css'
 
 export function Home() {
@@ -7,17 +8,7 @@ export function Home() {
         <main className='home-main'>
             <h1 className='home-h1'>Welcome to The News Wave!</h1>
 
-            <div className='cards-container'>
-                <section className='article-card'>
-                    <img 
-                    src="https://nationalseniors.com.au/generated/1440w-3-2/istock-1294303237-cryptocurrency-investment-financial-22-jpg.jpg?1644369957" 
-                    alt="" />    
-                    <div className="article-card__info">
-                        <h2 className='article-card__title'>Crypto currency</h2>
-                        <button className='article-card__gotobtn'>Topic</button>
-                    </div>
-                </section>                
-            </div>
+            <TrendingTopics />
 
             <div className='join-container'>
                 <section className="join-card">

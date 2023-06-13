7c8e1b430cad591f94c47bb7d5cd4652b9024fdc
santiago-federici <santiagofederici88@gmail.com>
2023-06-13 18:30:34 -0300

    Added some NavLinks

--- src/components/mobileIcons/MobileIcons.jsx
+++ src/components/mobileIcons/MobileIcons.jsx
@@ -1,6 +1,7 @@
 import { AiOutlineHome } from 'react-icons/ai'
 import { GrArticle } from 'react-icons/gr'
 import { AiOutlineFolder } from 'react-icons/ai'
+import { NavLink } from 'react-router-dom'
 
 import './mobileicons.css'
 
@@ -8,9 +9,11 @@ export function MobileIcons(){
 
     return(
         <section className='mobileicons-container'>
-            <AiOutlineHome className='aioutlinehome' />
-            <GrArticle className='grarticle' />
-            <AiOutlineFolder className='aioutlinefolder' />
+            <NavLink to={'/'}><AiOutlineHome className='aioutlinehome' /></NavLink>
+            <NavLink to={'/articles'}><GrArticle className='grarticle' /></NavLink>
+            <NavLink to={'/another'}><AiOutlineFolder className='aioutlinefolder' /></NavLink>
+            
+            
         </section>
     )
 }
--- src/components/readMore/ReadMore.jsx
+++ src/components/readMore/ReadMore.jsx
@@ -1,14 +1,28 @@
 import  User  from '../../assets/images/user.jpg'
 import Car from '../../assets/images/car.jpg'
+
 import { BiLike } from 'react-icons/bi'
+import { AiFillLike } from 'react-icons/ai'
 import { FaRegComment } from 'react-icons/fa'
-import { BsBookmark } from 'react-icons/bs'
-import { BsFillBookmarkFill } from 'react-icons/bs'
+import { BsBookmark, BsFillBookmarkFill } from 'react-icons/bs'
+import { HiArrowNarrowLeft } from 'react-icons/hi'
+
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
 import './readmore.css'
 
 export function ReadMore() {
+
+    let [isBookmarked, setIsBookmarked] = useState(false)
+    let [isLiked, setIsLike] = useState(false)
+    let likes = 15
+    let comments = 3
+
+
     return(
         <section className='readmore-card'>
+        <NavLink to={'/articles'}><HiArrowNarrowLeft className='back-articles-arrow' /></NavLink>
             <img src={ Car } alt="" />
             <div className='readmore-card__info'>
                 <div className='readmore-card__user'>
@@ -21,12 +35,32 @@ export function ReadMore() {
                 <h3>Another title</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia perferendis soluta ipsa magni obcaecati nostrum possimus ipsam reiciendis facilis. Delectus, nam quod. Dolor dolorum, harum laudantium eius ex voluptatibus necessitatibus, ea, nihil reiciendis ad facilis amet voluptas illum mollitia. Perferendis!</p>
                 <div className='interaction-icons-container'>
-                    <BiLike className='bilike' />
-                    <p>15</p>
+                    {
+                        isLiked
+                        ?
+                        <AiFillLike className='aifilllike' onClick={() => {
+                            setIsLike(false)
+                        }} />
+                        :
+                        <BiLike className='bilike' onClick={() => {
+                            setIsLike(true)
+                        }} />
+                    }
+                    <p>{likes}</p>
                     <FaRegComment className='faregcomment' />
-                    <p>3</p>
-                    <BsBookmark className='bsbookmark' />
-                    {/* <BsFillBookmarkFill className='bsfillbookmarkfill' /> */}
+                    <p>{comments}</p>
+                    {
+                        isBookmarked
+                        ?
+                        <BsFillBookmarkFill className='bsfillbookmarkfill' onClick={() => {
+                            setIsBookmarked(false)
+                        }} />
+                         :
+                        <BsBookmark className='bsbookmark' onClick={() => {
+                            setIsBookmarked(true)
+                        }} />
+                    }
+                    {/* <BsBookmark className='bsbookmark' /> */}
                 </div>
             </div>
         </section>
